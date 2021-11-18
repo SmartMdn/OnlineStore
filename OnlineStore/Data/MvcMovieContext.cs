fb74d316272af76f46bf4cb2c5b302637aa8f05c
vampi <vampi@DESKTOP-NT1U6RN>
2021-11-19 00:01:02 +0300

    Добавьте файлы проекта.

---
+++ OnlineStore/Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace OnlineStore.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+       
+    }
+}
