2ab44cf2fe8b983563509d2537e3ccf5102aeb56
Илья Матюнин <i.matyunin@gmail.com>
2021-11-25 15:00:59 +0300

    fixed validation

--- OnlineStore/Models/Product.cs
+++ OnlineStore/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,17 @@ namespace OnlineStore.WebUI.Models
     public class Product
     {
         public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Название товара")]
         public string Title { get; set; }
-
+        [Display(Name = "Дата производства")]
         [DataType(DataType.Date)]
         public DateTime ProductionDateTime { get; set; }
+        [Display(Name = "Цена")]
+        [Column(TypeName = "decimal(18, 2)")]
         public double Price { get; set; }
+        [Display(Name = "Описание")]
         public string Description { get; set; }
         
 
