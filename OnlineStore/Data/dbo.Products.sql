9b02b734ab4d6f8d0f1919d1a96ab64d0c84faec
Матюнин Илья <i.matyuninn@gmail.com>
2021-11-26 02:40:12 +0300

    база

---
+++ OnlineStore/Data/dbo.Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [Title]              NVARCHAR (MAX) NULL,
+    [ProductionDateTime] DATETIME2 (7)  NOT NULL,
+    [Price]              FLOAT (53)     NOT NULL,
+    [Description]        NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
